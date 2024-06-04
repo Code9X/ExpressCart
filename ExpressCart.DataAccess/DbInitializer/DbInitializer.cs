182040dd1d421d938a6c4c94bbc4c34ca6f4ec4c
Sebin Saji <sebinsaji0809@gmail.com>
2024-06-04 12:25:44 +0530

    Added Category Name as heading

--- ExpressCart.DataAccess/DbInitializer/DbInitializer.cs
+++ ExpressCart.DataAccess/DbInitializer/DbInitializer.cs
@@ -42,7 +42,7 @@ namespace ExpressCart.DataAccess.DbInitializer
 				throw;
 			}
 
-			//Create roles if they are not created 
+			//Create roles if they are not created
 			if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
 			{
 				_roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
